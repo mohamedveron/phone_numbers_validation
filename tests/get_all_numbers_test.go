d5c9fa7287bd58b677c37bb1b1ed44a87ca79c67
mohamed-mohaimen <mohamedveron23@gmail.com>
2021-02-22 13:48:03 +0200

    add all unit tests

--- tests/get_all_numbers_test.go
+++ tests/get_all_numbers_test.go
@@ -15,3 +15,38 @@ func TestGetAllNumbers(t *testing.T){
 		t.Error("error while getting phone numbers list")
 	}
 }
+
+func TestGetAllValidNumbers(t *testing.T){
+
+	persistenceLayer := persistence.NewPersistence("./sample.db")
+	serviceLayer := service.NewService(persistenceLayer, GetCountriesMap())
+	numbers, err := serviceLayer.GetPhoneNumbers("", "valid")
+
+	if err != nil || numbers.Data["Cameroon"].NotValidNumbers != nil{
+		t.Error("there must be only valid numbers in the response")
+	}
+}
+
+func TestGetSpecificCountryNumbers(t *testing.T){
+
+	persistenceLayer := persistence.NewPersistence("./sample.db")
+	serviceLayer := service.NewService(persistenceLayer, GetCountriesMap())
+	numbers, err := serviceLayer.GetPhoneNumbers("Cameroon", "")
+	_, exist := numbers.Data["Morocco"]
+
+	if err != nil || exist{
+		t.Error("there must be only numbers from just Cameroon country in the response")
+	}
+}
+
+func TestGetSpecificCountryValidNumbers(t *testing.T){
+
+	persistenceLayer := persistence.NewPersistence("./sample.db")
+	serviceLayer := service.NewService(persistenceLayer, GetCountriesMap())
+	numbers, err := serviceLayer.GetPhoneNumbers("Cameroon", "valid")
+	_, exist := numbers.Data["Morocco"]
+
+	if err != nil || exist || numbers.Data["Cameroon"].NotValidNumbers != nil{
+		t.Error("there must be only valid numbers from just Cameroon country in the response")
+	}
+}
